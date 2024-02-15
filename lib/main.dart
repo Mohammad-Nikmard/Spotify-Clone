5db982f7ddf6684b0972800948790221c70d4e60
MohammadNikmard <mnikmard2004@gmail.com>
2024-02-15 10:43:20 +0330

    add create password screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:spotify_clone/ui/choose_artist_screen.dart';
-import 'package:spotify_clone/ui/create_email_screen.dart';
+import 'package:spotify_clone/ui/create_password_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CreateEmailScreen(),
+      home: CreatePasswordScreen(),
     );
   }
 }
